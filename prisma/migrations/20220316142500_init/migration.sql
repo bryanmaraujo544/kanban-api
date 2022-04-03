a02829cc21f76e8a23158fc94d18b951894e9083
Bryan Martins <bryanmaraujo544@gmail.com>
2022-04-03 17:58:25 -0300

    fix: fixing the prisma issue

--- prisma/migrations/20220316142500_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `users` MODIFY `photo_url` VARCHAR(1024) NOT NULL;
--- prisma/migrations/20220319201605_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `index` to the `tasks` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `tasks` ADD COLUMN `index` INTEGER NOT NULL;
