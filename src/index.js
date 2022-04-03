36546d624848b3b882b3d87fbd2c928f6528fcb6
Bryan Martins <bryanmaraujo544@gmail.com>
2022-04-03 12:45:27 -0300

    fix: adding error handler and changing the url of the database

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const errorHandler = require('./app/middlewares/errorHandler');
 
 const { Server } = require('socket.io');
 const io = new Server(server, {
@@ -21,6 +22,7 @@ io.on('connection', (socket) => {
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
